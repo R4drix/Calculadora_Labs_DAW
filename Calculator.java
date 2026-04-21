c7be654576f419269514052d59369221ddfe8242
Iker <n0ch.n3ywl@gmail.com>
2026-04-21 10:21:03 -0500

    Suma y Resta v2

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,26 @@
 class Calculator {
 
+    public int add(int a, int b){ 
 
-    int add(int a, int b){ return 0; }
-    int sub(int a, int b){ return 0; }
+        return a+b; 
 
-        public int mul(int a, int b) {
-            return a * b;
-        }
+    }
+    public int sub(int a, int b){
+
+        return a-b; 
+
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
         
-        public int div(int a, int b) {
-            if (b == 0) {
-                throw new IllegalArgumentException("No se puede dividir por cero");
-            }
-            return a / b;
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
         }
+        return a / b;
+    }
 
     int mod(int a, int b){ return 0; }
 }
