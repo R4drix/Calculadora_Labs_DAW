e9dc58cddf3606906c301434dd456a5503608ba0
ivan-SalazarL <ssalazarl@unsa.edu.pe>
2026-04-23 10:08:57 -0500

    Ivan: Implementacion de sub y mod

--- Calculator.java
+++ Calculator.java
@@ -1,24 +1,33 @@
 class Calculator {
     private int num1;
     private int num2;
+
     public Calculator(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
-    int add(int a, int b){ return 0; }
-    int sub(int a, int b){ return 0; }
+    int add(int a, int b) { 
+        return 0; 
+    }
 
-        public int mul(int a, int b) {
-            return a * b;
-        }
-        
-        public int div(int a, int b) {
-            if (b == 0) {
-                throw new IllegalArgumentException("No se puede dividir por cero");
-            }
-            return a / b;
-        }
+    int sub(int a, int b) { 
+        return a - b; 
+    }
 
-    int mod(int a, int b){ return 0; }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+
+    int mod(int a, int b) { 
+        if (b == 0) return 0;
+        return a % b; 
+    }
 }
