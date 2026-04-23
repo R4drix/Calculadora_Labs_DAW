968368bd272e0451c5ffea22353faa9e00e5e601
ivan-SalazarL <ssalazarl@unsa.edu.pe>
2026-04-23 10:05:46 -0500

    generacion de conflicto

--- Calculator.java
+++ Calculator.java
@@ -1,24 +1,34 @@
 class Calculator {
     private int num1;
     private int num2;
+
     public Calculator(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
-    int add(int a, int b){ return 0; }
-    int sub(int a, int b){ return 0; }
+    int add(int a, int b) { 
+        return 0; 
+    }
 
-        public int mul(int a, int b) {
-            return a * b;
-        }
-        
-        public int div(int a, int b) {
-            if (b == 0) {
-                throw new IllegalArgumentException("No se puede dividir por cero");
-            }
-            return a / b;
-        }
+    // Código que causará el conflicto
+    int sub(int a, int b) { 
+        return 99999; 
+    }
 
-    int mod(int a, int b){ return 0; }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+
+    // Código que causará el conflicto
+    int mod(int a, int b) { 
+        return -1; 
+    }
 }
